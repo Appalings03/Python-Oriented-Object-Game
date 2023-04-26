39e764b486c0ca34e64be85446f49744d5fe2bf1
Martin Binamé <marti>
2023-04-26 14:38:57 +0200

    Implementation Game Object Methode

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-class GameObject:
-    name = ""
-    apperance = ""
-    feel = ""
-    smell = ""
-    
+class GameObject:    
     def __init__(self, name, apperance, feel, smell):
         self.name = name
         self.apperance = apperance
         self.feel = feel
         self.smell = smell
-
-
     
+    def look(self):
+        return f"You look at the {self.name}. {self.apperance}\n"
+
+    def touch(self):
+        return f"You touch the {self.name}. {self.feel}\n"
+
+    def sniff(self):
+        return f"You sniff the {self.name}. {self.smell}\n"
+
+game_object = GameObject("Knife","Some appareance","Some feel", "Some smell") 
+
+game_object.name = "Spoon"
+print(game_object.sniff())
