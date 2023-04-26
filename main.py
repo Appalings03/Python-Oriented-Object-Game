ea515bcf7f13ecb3166ff0f8bb66df38de107672
Martin Binamé <marti>
2023-04-26 14:50:11 +0200

    Room Class Implementation

--- main.py
+++ main.py
@@ -14,7 +14,26 @@ class GameObject:
     def sniff(self):
         return f"You sniff the {self.name}. {self.smell}\n"
 
+'''
 game_object = GameObject("Knife","Some appareance","Some feel", "Some smell") 
 
 game_object.name = "Spoon"
 print(game_object.sniff())
+'''
+
+class Room :
+    escape_code = 0
+    game_objects = []
+
+    def __init__(self, escape_code,game_objects):
+        self.escape_code = escape_code
+        self.game_objects = game_objects
+
+    def check_code(self, code):
+        return self.escape_code == code
+
+    def get_game_object_names(self):
+        names = []
+        for object in self.game_objects:
+            names.append(object.name)
+        return names
